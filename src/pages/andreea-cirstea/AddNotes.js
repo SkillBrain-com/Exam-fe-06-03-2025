244a899e124af14e2bac2c95ab2e67a260f65b84
Andreea Cirstea <intrendbydeea@gmail.com>
2025-03-06 20:45:19 +0200

    modificare

--- src/pages/andreea-cirstea/AddNotes.js
+++ src/pages/andreea-cirstea/AddNotes.js
@@ -3,13 +3,13 @@ import AddNote from "./AddNote";
 import NoteCard from "../../components/NoteCard";
 
 const initialNote = {
-  title: "First Note",
+  title: "To do list",
   content: "This is my first note",
 };
 
 function AddNotes() {
   return (
-    <div>
+    <div style={{ maxWidth: "600px", margin: "40px auto", padding: "20px", borderRadius: "10px", boxShadow: "0px 4px 10px rgba(0,0,0,0.1)", background: "#5F9964" }}>
       <AddNote />
       <NoteCard title={initialNote.title} content={initialNote.content} />
     </div>

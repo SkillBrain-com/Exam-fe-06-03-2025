5862f2d53039db4020c35ec831967bf3cc50432c
Lungu Marina-Diana <marinadiana1999@yahoo.com>
2025-03-06 21:27:24 +0200

    implementare-aplicatie-notite

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MentorNotes from "./pages/mentor/AddNotes";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import AddNotes from "./pages/marina-diana_lungu/AddNotes";
+import AddNotes from "./pages/lungu_marina-diana/AddNotes";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <h1>Note App</h1>
         <Routes>
           <Route path="/" element={<MentorNotes />} />
-          <Route path="/marina-diana_lungu" element={<AddNotes />} />
+          <Route path="/lungu_marina-diana" element={<AddNotes />} />
           {/* Add your route here */}
         </Routes>
       </div>
